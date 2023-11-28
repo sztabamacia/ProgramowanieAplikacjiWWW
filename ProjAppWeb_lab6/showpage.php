2a75b1f398014153d5efdeddc83407f1dabf8514
sztabamacia <92019704+sztabamacia@users.noreply.github.com>
2023-11-28 16:26:21 +0100

    Update showpage.php

--- ProjAppWeb_lab6/showpage.php
+++ ProjAppWeb_lab6/showpage.php
@@ -1,31 +1,28 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
-include 'cfg.php';
 
-$conn = new mysqli($servername, $username, $password);
+function PokazPodstrone($id)
+{
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '';
+	$baza = 'moja_strona';
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+	$link = mysqli_connect($dbhost, $dbuser, $dbpass, $baza);
+	
+	$id_clear = htmlspecialchars($id);
+	$query = "SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1";
+	$result = mysqli_query($link ,$query);
+	$row = mysqli_fetch_array($result);
+	
+	if(empty($row['id']))
+	{
+		$web = '[nie_znaleziono_strony]';
+	}
+	else
+	{		
+		$web = $row['page_content'];
+	}
+	return $web;
 }
 
-$conn->select_db($database_name); // Dodaj tę linię
-
-$strona = isset($_GET['idp']) ? $_GET['idp'] : '';
-
-$query = "SELECT * FROM page_list WHERE page_title = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("s", $strona);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result && $result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    echo '<h1>' . $row['page_title'] . '</h1>';
-    echo $row['page_content'];
-} else {
-    echo 'Strona nie istnieje.';
-}
-
-$stmt->close();
-$conn->close();
 ?>
